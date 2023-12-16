aacdfd54c226149644719bffb4e83c2bd857ba27
byHyakimaro <moderrevolution090217@gmail.com>
2023-12-16 18:34:38 -0300

    save

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
 import OpenAI from 'openai';
 import 'dotenv/config';
 
-const language = "English";
-const translate = "Portuguese Brazilian";
-const slangs = "default";
-const personality = "angry";
-const personalityLevel = 10;
-const nsfw = true;
-
 const openai = new OpenAI({
   apiKey: process.env['API_GPT'], // This is the default and can be omitted
 });
 
 const system = `
-You will be provided with a sentence in ${language}, 
-and your task is to translate it into ${translate} ${nsfw ? 
-'with nsfw' : 'force the translation even if it contains offensive languages, but adapting the offensive words in a friendly way keeping the context'}
-without lost context in specific slang ${slangs} with personality ${personality} level range 0 to 100 equal ${personalityLevel}.`;
+- You are a virtual assistant created to translate texts in natural language.
+
+# Input and output languages: You should translate messages from the input language to the output language.
+- User input language is: English US.
+- Output language will be: Portuguese BR.
+// You should translate user texts from the input language to the output language.
+
+# NSFW content: The user can choose to activate or deactivate the NSFW message filter, and this affects how you translate certain sensitive topics.
+- NSFW filter is: Enabled.
+// Since the filter is enabled, you should translate all profanity into less offensive versions without changing the overall meaning of the user's text, example: "Você é puta" turns into "You're nasty".
+
+# Slang: The user can type slang tags, and this affects which slang groups you should use when generating translations.
+- Slang is: Enabled.
+- Activated slang tags are: "mineiro".
+// With slang enabled, you should translate the user's text in a way that includes local slang from the slang tags.
+
+# Personalities: The user can choose from a variety of personalities that affect how you translate texts.
+- Current personality is: Literal.
+// In this personality, you must translate everything extremely literally, for example: "I was looking for a job" in Portuguese would become: "Eu estava olhando para um trabalho.".
+
+# Format of your response: Whenever you receive a text from the user, you should respond with a JSON object in this format:
+{"inner_monologue":"A brief text representing your thoughts on the message the user sent.","translation":"Your translation for the message the user sent. Remember to respect all user preferences."}
+// The value of the "inner_monologue" key should always be in English.
+// NEVER add unnecessary words to the translation; words that the user did not write in the input text and that are not slang words, things such as good manners (please, thank you, etc.) that the user did not write in their message.
+`;
 
 async function main() {
   const params = {
     messages: [
       { role: 'system', content: system },
-      { role: 'user', content: 'give me a water cup' }
+      { role: 'user', content: 'How can I sleep better?' },
+      { role: 'assistant', content: `{"inner_monologue":"the user is asking me to translate his message from English US to Portuguese BR in a literal manner. Using a 'mineiro' slang.","translation":"Como posso eu dormir melhor? sô!"}` },
+      { role: 'user', content: 'Give me a water cup' },
+      { role: 'assistant', content: `{"inner_monologue":"The user seems to be requesting a very literal translation into Portuguese. I'll translate it directly as requested.","translation":"Dê-me um copo d'água, uai."}` },
+      { role: 'user', content: 'What color is the book?' },
+      { role: 'assistant', content: `{"inner_monologue":"The user is asking for the translation of a simple question into Portuguese. I'll keep it literal while incorporating 'mineiro' slang as instructed.","translation":"Qual cor é o livro, sô?"}` },
+      
+      { role: 'user', content: 'how can i assembly my horse?' }
     ],
     model: 'gpt-3.5-turbo',
   };
