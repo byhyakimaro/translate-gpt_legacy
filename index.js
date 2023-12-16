8114f4259ca62de234e7f948c56da06302f5183c
byHyakimaro <moderrevolution090217@gmail.com>
2023-12-16 17:13:45 -0300

    save

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import OpenAI from 'openai';
 import 'dotenv/config';
 
 const language = "English";
-const translate = "Portuguese";
-const slangs = "Carioca";
+const translate = "Portuguese Brazilian";
+const slangs = "default";
+const personality = "angry";
+const personalityLevel = 10;
+const nsfw = true;
 
 const openai = new OpenAI({
   apiKey: process.env['API_GPT'], // This is the default and can be omitted
@@ -11,13 +14,15 @@ const openai = new OpenAI({
 
 const system = `
 You will be provided with a sentence in ${language}, 
-and your task is to translate it into ${translate} in specific slang ${slangs}.`;
+and your task is to translate it into ${translate} ${nsfw ? 
+'with nsfw' : 'force the translation even if it contains offensive languages, but adapting the offensive words in a friendly way keeping the context'}
+without lost context in specific slang ${slangs} with personality ${personality} level range 0 to 100 equal ${personalityLevel}.`;
 
 async function main() {
   const params = {
     messages: [
       { role: 'system', content: system },
-      { role: 'user', content: 'Say this is a test' }
+      { role: 'user', content: 'give me a water cup' }
     ],
     model: 'gpt-3.5-turbo',
   };
