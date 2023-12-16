e8f7f71eb369a3a5b753ebf47c7483479c38120e
byHyakimaro <moderrevolution090217@gmail.com>
2023-12-16 15:49:03 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import OpenAI from 'openai';
+import 'dotenv/config';
+
+const language = "English";
+const translate = "French";
+
+const openai = new OpenAI({
+  apiKey: process.env['API_GPT'], // This is the default and can be omitted
+});
+
+const system = `You will be provided with a sentence in ${language}, and your task is to translate it into ${translate}.`;
+
+async function main() {
+  const params = {
+    messages: [
+      { role: 'system', content: system },
+      { role: 'user', content: 'Say this is a test' }
+    ],
+    model: 'gpt-3.5-turbo',
+  };
+  const chatCompletion = await openai.chat.completions.create(params);
+  console.log(chatCompletion.choices[0]?.message?.content);
+}
+
+main();
